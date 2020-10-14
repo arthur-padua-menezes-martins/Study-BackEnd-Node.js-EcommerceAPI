f04e78c65bf3bc6f234da03fd7b2fd34baa65302
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-13 23:48:11 -0300

    feat: returns from httpResponde "{statusCode: 400}" if any fields do not exist < version: 0.0.3, principal path: src/presentation/controllers/sing-up/sing-up.ts >

--- src/presentation/controllers/sign-up/sign-up.ts
+++ src/presentation/controllers/sign-up/sign-up.ts
@@ -3,11 +3,17 @@ import { MissingParamError } from '../../errors/export-all'
 
 export class SignUpController {
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const httpRequestFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
+    const httpRequestBodyFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
+    const httpRequestBodyAddressFields: string[] = ['cep', 'street', 'number', 'neighborhood', 'city', 'state']
 
     var MissingFields: string = ''
-    for (const field of httpRequestFields) {
+    for (const field of httpRequestBodyFields) {
       MissingFields += !(field in httpRequest.body) ? `${field} ` : ''
+    }
+    if ('address' in httpRequest.body && httpRequest.body.address !== undefined) {
+      for (const addressField of httpRequestBodyAddressFields) {
+        MissingFields += !(addressField in httpRequest.body.address) ? `${addressField} ` : ''
+      }
     }
 
     return {
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,5 +1,18 @@
 export interface IHttpRequest {
-  body: object
+  body: {
+    name?: string
+    email?: string
+    password?: string
+    passwordConfirmation?: string
+    address?: {
+      cep?: string
+      street?: string
+      number?: string
+      neighborhood?: string
+      city?: string
+      state?: string
+    }
+  }
 }
 
 export interface IHttpResponse {
