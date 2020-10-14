860b5169f89fe096bf47a963562ded30f7cd2c87
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-14 20:03:25 -0300

    test: return from httpResponse "{status Code: 400}" if any fields do not match <version 0.0.1, principal path: src/presentation/controllers/sign-up/sing-up.ts>

--- src/presentation/controllers/sign-up/sign-up.spec.ts
+++ src/presentation/controllers/sign-up/sign-up.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from './sign-up'
 import { IHttpRequest } from './sign-up-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors/export-all'
+import { httpRequestBodyNotMatch, httpRequestBodyMissingField, httpRequestBodyInvalidPasswordConfirmation } from '../../helpers/httpRequestData'
 
 interface ISignUpControllerTypes {
   systemUnderTest: SignUpController
@@ -13,69 +14,51 @@ const makeSignUpController = (): ISignUpControllerTypes => {
   }
 }
 
+const httpRequestBodyFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
+const httpRequestBodyAddressFields: string[] = ['cep', 'street', 'number', 'neighborhood', 'city', 'state']
+
 describe('presentation/controllers/sign-up.spec.ts', () => {
   test('returns from httpResponde "{statusCode: 400}" if any fields do not exist <version 0.0.3>', async () => {
     const { systemUnderTest } = makeSignUpController()
-    const httpRequestBodyFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
-    const httpRequestBodyAddressFields: string[] = ['cep', 'street', 'number', 'neighborhood', 'city', 'state']
-    var MissingFields: string = ''
+    var missingFields: string = ''
 
-    const expectedHttpRequest: IHttpRequest = {
-      body: {
-        name: '@test-field-name-valid',
-        email: '@test-field-email-valid',
-        password: '@test-field-password-valid',
-        passwordConfirmation: '@test-field-password-valid',
-        address: {
-          cep: '@test-field-cep-valid',
-          street: '@test-field-street-valid',
-          number: '@test-field-number-valid',
-          neighborhood: '@test-field-neighborhood-valid',
-          city: '@test-field-city-valid',
-          state: '@test-field-state-valid'
-        }
-      }
-    }
     const httpRequest: IHttpRequest = {
-      body: {
-        email: '@test-field-email-valid',
-        password: '@test-field-password-valid',
-        passwordConfirmation: '@test-field-password-valid'
-      }
+      body: httpRequestBodyMissingField
     }
-    const expectedHttpRequestBodyLength = (Object.values(expectedHttpRequest.body)).length
-    const httpRequestBodyLength = (Object.values(httpRequest.body)).length
 
     for (const field of httpRequestBodyFields) {
-      MissingFields += !(field in httpRequest.body) ? `${field} ` : ''
+      missingFields += !(field in httpRequest.body) ? `${field} ` : ''
     }
     if ('address' in httpRequest.body && httpRequest.body.address !== undefined) {
       for (const addressField of httpRequestBodyAddressFields) {
-        MissingFields += !(addressField in httpRequest.body.address) ? `${addressField} ` : ''
+        missingFields += !(addressField in httpRequest.body.address) ? `${addressField} ` : ''
       }
     }
 
     const httpResponse = await systemUnderTest.handle(httpRequest)
-    expect(httpRequestBodyLength).not.toBe(expectedHttpRequestBodyLength)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.errorMessage).toEqual(new MissingParamError(MissingFields))
+    expect(httpResponse.errorMessage).toEqual(new MissingParamError(missingFields))
   })
 
-  test('return from httpResponse "{status Code: 400}" if password confirmation does not match <version 0.0.1>', async () => {
+  test('return from httpResponse "{status Code: 400}" if the password confirmation does not match the password <version 0.0.1>', async () => {
     const { systemUnderTest } = makeSignUpController()
     const httpRequest: IHttpRequest = {
-      body: {
-        password: '@test-field-password-valid',
-        passwordConfirmation: '@test-field-password-invalid'
-      }
+      body: httpRequestBodyInvalidPasswordConfirmation
     }
 
     const httpResponse = await systemUnderTest.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.errorMessage).toEqual(new InvalidParamError('passwordConfirmation'))
   })
-})
 
-// name RegEXP /^[a-zA-Z\u00C0-\u017F´]+\s+[a-zA-Z\u00C0-\u017F´]{0,}$/
-// email RegEXP /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-// password RegEXP /^(?=.*\d)(?=.*[a-zA-Z])(?!.*[\W_\x7B-\xFF]).{8,16}$/
+  test('return from httpResponse "{status Code: 400}" if any fields do not match <version 0.0.1>', async () => {
+    const { systemUnderTest } = makeSignUpController()
+
+    const httpRequest: IHttpRequest = {
+      body: httpRequestBodyNotMatch
+    }
+
+    const httpResponse = await systemUnderTest.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
