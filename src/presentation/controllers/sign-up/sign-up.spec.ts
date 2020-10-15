ebefdffee449b853373f7e2f0baf3ca63b62a27f
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-15 02:21:32 -0300

    test: must call AddAccount with the correct values <version 0.0.1>

--- src/presentation/controllers/sign-up/sign-up.spec.ts
+++ src/presentation/controllers/sign-up/sign-up.spec.ts
@@ -1,19 +1,38 @@
 import { SignUpController } from './sign-up'
-import { IHttpRequest } from './sign-up-protocols'
+import { IHttpRequest, IAddAccount, IAddAccountModel, IAccountModel } from './sign-up-protocols'
 import { MissingParamError, InvalidParamError, ServerError } from '../../errors/export-all'
 import {
   httpRequestBodyFields, httpRequestBodyAddressFields,
-  httpRequestBodyNotMatch, httpRequestBodyMissingField, httpRequestBodyInvalidPasswordConfirmation
+  httpRequestBodyNotMatch, httpRequestBodyMissingField, httpRequestBodyInvalidPasswordConfirmation,
+  httpRequestBodyMatchComplete
 } from '../../helpers/export-all'
+
+const makeAddAccount = (): IAddAccount => {
+  class AddAccountStub {
+    add (account: IAddAccountModel): IAccountModel {
+      const fakeAccount = {
+        id: '5f22f7d096695c2878f37d5f',
+        ...httpRequestBodyMatchComplete
+      }
+
+      return fakeAccount
+    }
+  }
+
+  return new AddAccountStub()
+}
 
 interface ISignUpControllerTypes {
   systemUnderTest: SignUpController
+  addAccountStub: IAddAccount
 }
 const makeSignUpController = (): ISignUpControllerTypes => {
-  const systemUnderTest = new SignUpController()
+  const addAccountStub = makeAddAccount()
+  const systemUnderTest = new SignUpController(addAccountStub)
 
   return {
-    systemUnderTest
+    systemUnderTest,
+    addAccountStub
   }
 }
 
@@ -40,7 +59,7 @@ describe('presentation/controllers/sign-up.spec.ts', () => {
     expect(httpResponse.errorMessage).toEqual(new MissingParamError(missingFields))
   })
 
-  test('return from httpResponse "{status Code: 400}" if the password confirmation does not match the password <version 0.0.1>', async () => {
+  test('returns from httpResponse "{status Code: 400}" if the password confirmation does not match the password <version 0.0.1>', async () => {
     const { systemUnderTest } = makeSignUpController()
     const httpRequest: IHttpRequest = {
       body: httpRequestBodyInvalidPasswordConfirmation
@@ -51,7 +70,7 @@ describe('presentation/controllers/sign-up.spec.ts', () => {
     expect(httpResponse.errorMessage).toEqual(new InvalidParamError('passwordConfirmation'))
   })
 
-  test('return from httpResponse "{status Code: 400}" if any fields do not match <version 0.0.1>', async () => {
+  test('returns from httpResponse "{status Code: 400}" if any fields do not match <version 0.0.1>', async () => {
     const { systemUnderTest } = makeSignUpController()
 
     const httpRequest: IHttpRequest = {
@@ -62,7 +81,7 @@ describe('presentation/controllers/sign-up.spec.ts', () => {
     expect(httpResponse.statusCode).toBe(400)
   })
 
-  test('return from httpResponse "{status Code: 500}" if validating any field returns an error <version 0.0.1>', async () => {
+  test('returns from httpResponse "{status Code: 500}" if validating any field throw an error <version 0.0.1>', async () => {
     const { systemUnderTest } = makeSignUpController()
 
     const httpRequest: IHttpRequest = {
@@ -75,5 +94,21 @@ describe('presentation/controllers/sign-up.spec.ts', () => {
     const httpResponse = await systemUnderTest.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.errorMessage).toEqual(new ServerError())
+  })
+
+  test('must call AddAccount with the correct values <version 0.0.1>', async () => {
+    const { systemUnderTest, addAccountStub } = makeSignUpController()
+    const spyOnAddAccountStubAdd = jest.spyOn(addAccountStub, 'add')
+
+    const httpRequest: IHttpRequest = {
+      body: httpRequestBodyMatchComplete
+    }
+
+    await systemUnderTest.handle(httpRequest)
+    expect(spyOnAddAccountStubAdd).toHaveBeenCalledWith(httpRequestBodyMatchComplete)
+  })
+
+  test('returns from httpResponse "{status Code: 500}" if AddAccount throw error <version 0.0.1>', async () => {
+
   })
 })
