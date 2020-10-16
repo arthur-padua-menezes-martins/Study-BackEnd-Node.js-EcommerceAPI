aa2ee4695701f7022d6478646fd34b7cec3d9968
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-16 16:31:04 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // refere-se o local onde serão encontrados os arquivos para testes
-    coverageDirectory: 'coverage',
-    testEnvironment: 'node', // ambiente de testes
-    transform: {
-      '.+\\.ts$': 'ts-jest' // utilização da lib ts-jest para converter o código .ts para .js e submete-lo ao jest
-    }
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // refere-se o local onde serão encontrados os arquivos para testes
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node', // ambiente de testes
+  transform: {
+    '.+\\.ts$': 'ts-jest' // utilização da lib ts-jest para converter o código .ts para .js e submete-lo ao jest
   }
-  
+}
