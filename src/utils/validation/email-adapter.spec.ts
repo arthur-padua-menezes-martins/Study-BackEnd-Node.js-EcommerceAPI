df844c2a68a7ebab28f8585626a7746eed331c14
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-17 12:01:30 -0300

    test: Should return true if validator returns true <version: 0.0.1>

--- src/utils/validation/email-adapter.spec.ts
+++ src/utils/validation/email-adapter.spec.ts
@@ -1,11 +1,31 @@
 import { EmailValidatorAdapter } from './email-adapter'
-import { getHttpRequestBodyNotMatchField } from '../../presentation/helpers/export-all'
+import { getHttpRequestBodyMatchField, getHttpRequestBodyNotMatchField } from '../../presentation/helpers/export-all'
 
-describe('EmailValidator Adapter', () => {
+interface IEmailValidatorAdapterTypes {
+  systemUnderTest: EmailValidatorAdapter
+}
+const makeEmailValidatorAdapter = async (): Promise<IEmailValidatorAdapterTypes> => {
+  const systemUnderTest = new EmailValidatorAdapter()
+
+  return {
+    systemUnderTest
+  }
+}
+
+describe('EmailValidatorAdapter', () => {
   test('Should return false if validator returns false <version: 0.0.1>', async () => {
-    const systemUnderTest = new EmailValidatorAdapter()
+    const { systemUnderTest } = await makeEmailValidatorAdapter()
+    jest.spyOn(systemUnderTest, 'email').mockReturnValueOnce(Promise.resolve(false))
     const isValid = await systemUnderTest.email(getHttpRequestBodyNotMatchField('email'))
 
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true <version: 0.0.1>', async () => {
+    const { systemUnderTest } = await makeEmailValidatorAdapter()
+    jest.spyOn(systemUnderTest, 'email').mockReturnValueOnce(Promise.resolve(true))
+    const isValid = await systemUnderTest.email(getHttpRequestBodyMatchField('email'))
+
+    expect(isValid).toBe(true)
   })
 })
