d7d1161d24a836c2d2957ef04ee466777416a9dc
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-17 20:47:52 -0300

    feat:

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,16 +1,24 @@
-import { IAddAccount, IAddAccountModel } from '../../../domain/usecases/add-account'
-import { IAccountModel } from '../../../domain/models/account'
-import { Encrypter } from '../../protocols/encrypter'
+import {
+  IAddAccount, IAddAccountModel, IAccountModel,
+  IEncrypter, IAddAccountRepository
+} from './db-add-account-protocols'
 
 export class DatabaseAddAccount implements IAddAccount {
-  private readonly encrypter: Encrypter
+  private readonly encrypter: IEncrypter
+  private readonly addAccountRepository: IAddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: IEncrypter, addAccountRepository: IAddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
-  async add (account: IAddAccountModel): Promise<IAccountModel> {
-    await this.encrypter.encrypt(account.password)
-    return await Promise.resolve(null)
+  async add (accountData: IAddAccountModel): Promise<IAccountModel> {
+    const encryptedPassword = await this.encrypter.encrypt(accountData.password)
+    const account = await this.addAccountRepository.add({
+      ...accountData,
+      password: encryptedPassword
+    })
+    console.log(account)
+    return await Promise.resolve(account)
   }
 }
