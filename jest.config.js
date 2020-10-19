cb642117084aaf2c4c57d7c49f6a85f8298fbf0b
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-19 13:50:59 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'], // refere-se o local onde serão encontrados os arquivos para testes
   coverageDirectory: 'coverage',
   testEnvironment: 'node', // ambiente de testes
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest' // utilização da lib ts-jest para converter o código .ts para .js e submete-lo ao jest
   }
