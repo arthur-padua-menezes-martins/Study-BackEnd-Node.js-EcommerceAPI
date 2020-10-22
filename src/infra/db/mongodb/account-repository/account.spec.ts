92b525b16576e6b47c44815edfe5aa20d7febc97
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-10-22 12:33:58 -0300

    test: Should return an account on success <version: 0.0.1 path: src/infra/db/mongodb/account-repository>

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,39 @@
-describe('AccountMongoRepository', () => {
-  test('', async () => {
+import { AccountMongoRepository } from './account'
+import { mongoHelper } from '../helper/mongo-helper'
+import {
+  httpRequestBodyFields, httpRequestBodyAddressFields,
+  httpRequestBodyMatchComplete
+} from '../../../../utils/fake-data/httpRequest'
 
+interface ISystemUnderTestTypes {
+  systemUnderTest: AccountMongoRepository
+}
+const makeSystemUnderTest = async (): Promise<ISystemUnderTestTypes> => {
+  const systemUnderTest = new AccountMongoRepository()
+
+  return {
+    systemUnderTest
+  }
+}
+
+describe('AccountMongoRepository', () => {
+  beforeAll(async () => {
+    await mongoHelper.connect('mongodb://localhost:27017')
+  })
+  afterAll(async () => {
+    await mongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const { systemUnderTest } = await makeSystemUnderTest()
+    const account = await systemUnderTest.add(httpRequestBodyMatchComplete)
+
+    expect(account).toBeTruthy()
+    for (const key of httpRequestBodyFields.slice(0, 2)) {
+      expect(httpRequestBodyMatchComplete[key]).toEqual(account[key])
+    }
+    for (const key of httpRequestBodyAddressFields) {
+      expect(httpRequestBodyMatchComplete[key]).toEqual(account[key])
+    }
   })
 })
