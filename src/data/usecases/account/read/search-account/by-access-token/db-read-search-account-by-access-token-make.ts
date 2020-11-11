3e0573d55e394d5eef83e2eec0043acdfd5d7a62
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-11 17:51:23 -0300

    feat: should call searchByAccessToken with correct values <version: 0.0.1>

--- src/data/usecases/account/read/search-account/by-access-token/db-read-search-account-by-access-token-make.ts
+++ src/data/usecases/account/read/search-account/by-access-token/db-read-search-account-by-access-token-make.ts
@@ -1,3 +1,6 @@
 export {
   makeDecrypterCryptography
 } from '../../../../../make/cryptography/decrypter/make-decrypter-cryptography'
+export {
+  makeReadAccount
+} from '../../../../../make/account/read/make-read-account'
--- src/data/usecases/account/read/search-account/by-access-token/db-read-search-account-by-access-token.ts
+++ src/data/usecases/account/read/search-account/by-access-token/db-read-search-account-by-access-token.ts
@@ -5,10 +5,19 @@ import {
 } from './db-read-search-account-by-access-token-protocols'
 
 export class DatabaseSearchAccountByAccessToken implements ISearchAccountByAccessTokenRepository {
-  constructor (private readonly decrypter: IDecrypter) {}
+  constructor (
+    private readonly decrypter: IDecrypter,
+    private readonly accountRepositoryRead: ISearchAccountByAccessTokenRepository
+  ) {}
 
   async searchByAccessToken (accessToken: string, role?: string): Promise<IAccountModel | null> {
-    await this.decrypter.decrypt(accessToken)
+    const token = await this.decrypter.decrypt(accessToken)
+
+    if (token) {
+      const account = await this.accountRepositoryRead.searchByAccessToken(token)
+      return account
+    }
+
     return null
   }
 }
