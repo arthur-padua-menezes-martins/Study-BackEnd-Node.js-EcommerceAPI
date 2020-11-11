574e0d9231ad807f1aeb743b99e21a2c77d0ad94
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-11 11:36:28 -0300

    test: return from httpResponse "{statusCode: 403}" if no x-access-token exists in headers <version: 0.0.1>

---
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -0,0 +1,34 @@
+import {
+  AuthMiddleare
+} from './auth-middleware'
+import {
+  IHttpRequest
+} from './auth-middleware-protocols'
+
+interface ISystemUnderTestTypes {
+  systemUnderTest: AuthMiddleare
+}
+const makeSystemUnderTest = async (): Promise<ISystemUnderTestTypes> => {
+  const systemUnderTest = new AuthMiddleare()
+
+  return {
+    systemUnderTest
+  }
+}
+
+const httpRequest: IHttpRequest['headers'] = {
+  headers: {
+
+  }
+}
+
+describe('Auth Middleware', () => {
+  test('return from httpResponse "{statusCode: 403}" if no x-access-token exists in headers <version: 0.0.1>', async () => {
+    const { systemUnderTest } = await makeSystemUnderTest()
+
+    const httpResponse = await systemUnderTest.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(403)
+    expect(httpResponse.errorMessage?.name).toBe('AccessDeniedError')
+  })
+})
