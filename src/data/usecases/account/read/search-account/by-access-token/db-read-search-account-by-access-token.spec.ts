db35da7e18c722487128f179b58684e9a60db78b
arthur-padua-menezes-martins <arthur.software.developer@gmail.com>
2020-11-11 17:51:34 -0300

    test: should call searchByAccessToken with correct values <version: 0.0.1>

--- src/data/usecases/account/read/search-account/by-access-token/db-read-search-account-by-access-token.spec.ts
+++ src/data/usecases/account/read/search-account/by-access-token/db-read-search-account-by-access-token.spec.ts
@@ -2,10 +2,12 @@ import {
   DatabaseSearchAccountByAccessToken
 } from './db-read-search-account-by-access-token'
 import {
-  IDecrypter
+  IDecrypter,
+  ISearchAccountByAccessTokenRepository
 } from './db-read-search-account-by-access-token-protocols'
 import {
-  makeDecrypterCryptography
+  makeDecrypterCryptography,
+  makeReadAccount
 } from './db-read-search-account-by-access-token-make'
 import {
   informationsOfAccessTokenHttpRequestHeaders
@@ -14,26 +16,48 @@ import {
 interface ISystemUnderTestTypes {
   systemUnderTest: DatabaseSearchAccountByAccessToken
   decrypterStub: IDecrypter
+  readAccountStub: ISearchAccountByAccessTokenRepository
 }
 const makeSystemUnderTest = async (): Promise<ISystemUnderTestTypes> => {
   const decrypterStub = await makeDecrypterCryptography()
-  const systemUnderTest = new DatabaseSearchAccountByAccessToken(decrypterStub)
+  const readAccountStub = await makeReadAccount()
+  const systemUnderTest = new DatabaseSearchAccountByAccessToken(decrypterStub, readAccountStub)
 
   return {
     systemUnderTest,
-    decrypterStub
+    decrypterStub,
+    readAccountStub
   }
 }
 
 const accessToken = informationsOfAccessTokenHttpRequestHeaders['x-access-token']
+const role = 'any_role'
 
 describe('DatabaseSearchAccountByAccessToken', () => {
-  test('should call Decrypter with correct values <version: 0.0.1>', async () => {
+  test('should call decrypter with correct values <version: 0.0.1>', async () => {
     const { systemUnderTest, decrypterStub } = await makeSystemUnderTest()
 
     const spyOnDecrypt = jest.spyOn(decrypterStub, 'decrypt')
-    await systemUnderTest.searchByAccessToken(accessToken)
+    await systemUnderTest.searchByAccessToken(accessToken, role)
 
-    expect(spyOnDecrypt).toHaveBeenCalledWith(accessToken)
+    expect(spyOnDecrypt).toHaveBeenCalledWith(accessToken, role)
+  })
+
+  test('should return null if decrypter return null <version: 0.0.1>', async () => {
+    const { systemUnderTest, decrypterStub } = await makeSystemUnderTest()
+
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(null)
+    const account = await systemUnderTest.searchByAccessToken(accessToken, role)
+
+    expect(account).toBeNull()
+  })
+
+  test('should call searchByAccessToken with correct values <version: 0.0.1>', async () => {
+    const { systemUnderTest, readAccountStub } = await makeSystemUnderTest()
+
+    const spyOnSearchByAccessToken = jest.spyOn(readAccountStub, 'searchByAccessToken')
+    await systemUnderTest.searchByAccessToken(accessToken, role)
+
+    expect(spyOnSearchByAccessToken).toHaveBeenCalledWith(accessToken, role)
   })
 })
